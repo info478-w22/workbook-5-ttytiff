e99d44a3bc8add391205c3675ab1d06eb45741da
ttytiff <59585119+ttytiff@users.noreply.github.com>
2022-03-18 20:22:02 -0700

    completed

--- analysis.R
+++ analysis.R
@@ -1,6 +1,35 @@
-# Workbook 6: analyze NHANES data
+# Tiffany Tse
+# Workbook 5: analyze NHANES data
 
 # Set up
-library(foreign)
+
 library(survey)
 library(Hmisc)
+
+demo <- sasxport.get("DEMO_I.XPT")
+alco <- sasxport.get("ALQ_I.XPT")
+
+nhanes <- merge(x = demo, y = alco, by = 'seqn', all = T)
+
+wt_sum <- sum(nhanes$wtint2yr, na.rm = T)
+
+# wt_sum: total US population
+
+# Part 2: Analysis
+nhanes$alq151[nhanes$alq151 == 2] <- 0
+nhanes$alq151[nhanes$alq151 == 7] <- NA
+nhanes$alq151[nhanes$alq151 == 9] <- NA
+
+
+# Survey Design
+nhanes_survey <- svydesign(
+  id = ~sdmvpsu,
+  nest = T,
+  strata = ~sdmvstra,
+  weights = ~wtint2yr,
+  data = nhanes
+)
+
+nhanes_mean <- svymean(nhanes_survey, mean)
+
+mean_by_gender <- svyby(~, ~riagender, dclus1, svymean)
